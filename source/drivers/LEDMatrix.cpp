ac8d83c781ecfda8a71fc5eca520a5c3e6c4ddc9
JoeBakalor <jmbakalor@gmail.com>
2020-09-25 07:24:29 -0400

    fix pull

--- source/drivers/LEDMatrix.cpp
+++ source/drivers/LEDMatrix.cpp
@@ -135,7 +135,7 @@ void LEDMatrix:: setLedMatrix(int row,int column)
     // Reset all pins
     for (int i = 0; i < matrixMap.columns; i++)
     {
-        matrixMap.columnPins[i]->getDigitalValue(PullNone);
+        matrixMap.columnPins[i]->getDigitalValue(None);
     }
     //matrixMap.columnPins[i]
 
