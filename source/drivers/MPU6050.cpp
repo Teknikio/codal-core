68c4886716ef38f4da0f64b2b5ff12272ba4c498
JoeBakalor <jmbakalor@gmail.com>
2021-05-20 15:38:12 -0400

    fix temperature

--- source/drivers/MPU6050.cpp
+++ source/drivers/MPU6050.cpp
@@ -86,7 +86,7 @@ int MPU6050::updateSample()
         gyro.z = (((i2cData[12] << 8) | i2cData[13]));
 
         int t = (((i2cData[6] << 8) | i2cData[7]));
-        temp = (float)(((35 + t) * 65536L)/100000);
+        temp = (float)(((float)t / 340) + 36.53)//(float)(((35 + t) * 65536L)/100000);
         //t * 10 / 34 + 3653;
         
         //t * 10 / 34 + 3653;
